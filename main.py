28258c036d9020700493a04cd651660c4357f8f4
Felony <186009960+Fe10ny@users.noreply.github.com>
2025-06-07 14:51:57 +0000

    main.py update

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+import requests
+
+# Gets Universe id from Place id
+def getUniverseId(i):
+    r = requests.get(f"https://apis.roblox.com/universes/v1/places/{i}/universe")
+    js = r.json()
+    print(js)
+    return js['universeId']
+
+def getData(i):
+    r = requests.get(f"https://games.roblox.com/v1/games?universeIds={i}")
+    js = r.json()
+    print(js)
+    return js['data'][0]['name']
+
 def Main():
     i = input("Experience Id:")
-
-    with open(f"{int(i)}.url", "w") as f:
+    universeId = getUniverseId(i)
+    name = getData(universeId)
+    with open(f"{name}.url", "w") as f:
         f.write("[InternetShortcut]\nIDList=\n")
         f.write(f"URL=roblox://placeId={i}")
     Main()
