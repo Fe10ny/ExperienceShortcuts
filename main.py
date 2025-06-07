b1246e826bf04c2567a2cb2523ae69f3d062e26f
Felony <186009960+Fe10ny@users.noreply.github.com>
2025-06-07 14:52:30 +0000

    added comment

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def getUniverseId(i):
     print(js)
     return js['universeId']
 
+# Gets Place name from Place id
 def getData(i):
     r = requests.get(f"https://games.roblox.com/v1/games?universeIds={i}")
     js = r.json()
