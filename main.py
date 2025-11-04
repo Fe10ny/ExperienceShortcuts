a5ca506f91bf5f49d8b49341bc06376c4775f9c3
Felony <186009960+Fe10ny@users.noreply.github.com>
2025-11-04 11:16:14 +0000

    private servers update

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ settingsjson = json.load(file)
 
 added_imports = {"requests": False, "pillow": False}
 
+# checks if packages are missing and sets variables
 try:
     import requests
     added_imports['requests'] = True
@@ -29,8 +30,7 @@ except ImportError as error:
     settingsjson['downloadExperienceIcon'] = False
     pass
 
-print(added_imports)
-print(settingsjson)
+
 # list of symbols that cannot be used for filename
 badchars = ["\\","/",":","*","?","\"","<",">","|"]
 
@@ -64,6 +64,43 @@ def getData(i):
     js = r.json()
     return js['data'][0]['name']
 
+# Gets private server id out of "privateServerLinkCode" variable in the link.
+def getPrivateServer():
+    i = input("Enter your private server link/code or leave empty to cancel: ")
+    if i == '':
+        return ''
+    if i.isdigit() is False:
+        # Extracts Experience id from game link.
+        result = re.search('privateServerLinkCode=(.*)#', i)
+        if result:
+            print(f"Got Private Server Id from Link {result.group(1)}")
+            i = result.group(1)
+            return i
+        else:
+            print("Error: Entered Experience Id is Invalid.")
+            getPrivateServer()
+    else:
+        return ''
+
+# returns contents for "URL=" line
+def updateWriteUrl(id, PrivateID):
+    send = f'roblox://place={id}'
+    if PrivateID != '':
+        send += f'&linkCode={PrivateID}'
+    return send
+
+def writeDeepLink(name, i, PrivateID):
+    with open(f"{name}.url", "w") as f:
+        f.write("[InternetShortcut]\nIDList=\n")
+        if PrivateID == None:
+            f.write(f"URL=roblox://navigation/share_links?code={i}&type=Server")
+        else:
+            urlwrite = updateWriteUrl(i, PrivateID)
+            f.write(f"URL={urlwrite}\n")
+        if settingsjson["downloadExperienceIcon"]:
+            icon_file = downloadIcon(i)
+            f.write(f"IconFile={icon_file}\nIconIndex=0\nHotKey=0")
+        f.close()
 
 def Main():
     i = input("Experience Id: ")
@@ -75,35 +112,52 @@ def Main():
             print(f"Got Experience Id from Link {result.group(1)}")
             i = result.group(1)
         else:
-            print("Error: Entered Experience Id is Invalid.")
-            Main()
-    match settingsjson['nameMode']:
-        case -1:
-            name = input("Enter a Name for your Deep Link: ")
-        case 0:
-            name = i
-        case 1:
-            universeId = getUniverseId(i)
-            if universeId == None:
-                print("Error: Invalid Experience id")
+            result = re.search("\\?code=(.*)&", i)
+            if result:
+                print("Detected private server id")
+                settingsjson['privateServer'] = False
+                settingsjson['downloadExperienceIcon'] = False
+                i = result.group(1)
+            else:
+                print("Error: Entered Experience Id is Invalid.")
                 Main()
-            name = getData(universeId)
-        case default:
-            print("Warning: Invalid 'nameMode' variable.\nThe Deep link name will be set to Experience Id...")
+    name = "0"
+    PrivateID = ''
+    if i.isdigit():
+        match settingsjson['nameMode']:
+            case -1:
+                name = input("Enter a Name for your Deep Link: ")
+            case 0:
+                name = i
+            case 1:
+                universeId = getUniverseId(i)
+                if universeId == None:
+                    print("Error: Invalid Experience id")
+                    Main()
+                name = getData(universeId)
+            case default:
+                print("Warning: Invalid 'nameMode' variable.\nThe Deep link name will be set to Experience Id...")
+                name = i
+    else:
+        input_name = input("Enter a Name for your Private Server Deep Link or leave empty for default: ")
+        if input_name != '':
+            name = input_name
+        else:
             name = i
+        PrivateID = None
+
+    if settingsjson['privateServer'] == True:
+        print("Would you like to enter Private Server link/code? \n     Y - Yes      N - No")
+        private_i = input("> ")
+        if private_i.upper() == "Y":
+            PrivateID = getPrivateServer()
 
     # Cleans String from bad symbols.
     for x in badchars:
         name = name.replace(x, '')
 
     # Creates an Deep link file.
-    with open(f"{name}.url", "w") as f:
-        f.write("[InternetShortcut]\nIDList=\n")
-        f.write(f"URL=roblox://placeId={i}\n")
-        if settingsjson["downloadExperienceIcon"]:
-            icon_file = downloadIcon(i)
-            f.write(f"IconFile={icon_file}\nIconIndex=0\nHotKey=0")
-        f.close()
+    writeDeepLink(name, i, PrivateID)
 
     Main()
 
